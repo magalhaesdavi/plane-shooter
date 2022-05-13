1a9337e2139454a02edf6ed5d69a7d3de5a1fca1
Wagno Leao Sergio <wagnoleao@gmail.com>
2022-05-13 13:12:55 -0300

    métodos scenario.update e scenario.reset implementados

--- works/T1/scenario.js
+++ works/T1/scenario.js
@@ -6,8 +6,34 @@ import { createGroundPlaneWired } from "../../libs/util/util.js";
 */}
 export class Scenario {
 	constructor(height, width) {
+		this.clock = 1;
+		this.threshold = width/2;
 		this.height = height;
 		this.width = width;
 		this.plane = createGroundPlaneWired(height, width);
+		this.second_plane = createGroundPlaneWired(height, width);
+		this.second_plane.position.set(0, 0, 2*width);
+	}
+
+	update(cameraHolder) {
+		if (this.clock) {
+			if (cameraHolder.position.z - this.threshold <= (this.plane.position.z)) {
+				this.second_plane.position.set(0, 0, this.plane.position.z - (this.width / 2));
+				this.clock = 0;
+			}
+		}
+		else {
+			if (cameraHolder.position.z - this.threshold <= (this.second_plane.position.z)) {
+				this.plane.position.set(0, 0, this.second_plane.position.z - (this.width / 2));
+				this.clock = 1;
+			}			
+		}
+		return;
+	}
+
+	reset() {
+		this.plane.position.set(0.0, 0.0, -0.02);
+		this.second_plane.position.set(0.0, 0.0, -0.02);
+		return;
 	}
 }
