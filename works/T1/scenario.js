428e71d003fdd5cf2d05f0efe4ffe1542636a464
Wagno Leao Sergio <wagnoleao@gmail.com>
2022-05-13 11:16:25 -0300

    Classe Scenario criado

--- works/T1/scenario.js
+++ works/T1/scenario.js
@@ -1,1 +1,13 @@
-import * as THREE from  'three';
+import { createGroundPlaneWired } from "../../libs/util/util.js";
+
+{/*
+	Classe referente a geração do ambiente do jogo e
+	armazenamento das informações do mesmo.
+*/}
+export class Scenario {
+	constructor(height, width) {
+		this.height = height;
+		this.width = width;
+		this.plane = createGroundPlaneWired(height, width);
+	}
+}
