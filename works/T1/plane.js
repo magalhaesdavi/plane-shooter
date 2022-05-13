90d83bb6e68d85b5c599e3ef5d45649d826beaab
Wagno Leao Sergio <wagnoleao@gmail.com>
2022-05-13 11:32:38 -0300

    Comentarios

--- works/T1/plane.js
+++ works/T1/plane.js
@@ -1,9 +1,13 @@
 import * as THREE from  'three';
 
+{/**
+    Classe referente ao objeto que controla as
+    funcionalidades do avião.
+*/}
 export class Plane {
     constructor() {
-        this.geometry = new THREE.ConeGeometry( 5, 20, 32 );
-        this.material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+        this.geometry = new THREE.ConeGeometry( 2, 3, 32 );
+        this.material = new THREE.MeshBasicMaterial();
         this.cone = new THREE.Mesh( this.geometry, this.material );
     }
 }
