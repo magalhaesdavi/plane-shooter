e9fac132409d00b3229b9922113a1dc26354f80b
Wagno Leao Sergio <wagnoleao@gmail.com>
2022-06-22 14:34:38 -0300

    Classe Bullet movida

--- works/T2/plane.js
+++ works/T2/plane.js
@@ -1,5 +1,6 @@
 import * as THREE from  'three';
 import { degreesToRadians } from '../../libs/util/util.js';
+import { Bullet } from './bullets.js';
 
 {/**
     Classe referente ao objeto que controla as
@@ -43,35 +44,5 @@ export class Airplane {
         else {
             return null;
         }
-    }
-}
-
-{/**
-    Classe referente ao objeto que controla as
-    funcionalidades dos projetes que o avião atira'.
-*/}
-export class Bullet {
-    constructor(speed) {
-        this.geometry = new THREE.SphereGeometry( 0.75, 32, 32 );
-        this.material = new THREE.MeshLambertMaterial( { color: 0xffa500 } );
-        this.sphere = new THREE.Mesh( this.geometry, this.material );
-        this.speed = speed * 2.25;
-        this.boundingBox = new THREE.Box3().setFromObject(this.sphere);
-        // this.sphere.geometry.computeBoundingBox();
-        return;
-    }
-
-    create(airplane) {
-        this.sphere.position.set(
-            airplane.cone.position.x,
-            airplane.cone.position.y,
-            airplane.cone.position.z - 5
-        );
-        return;
-    }
-
-    update() {
-        this.sphere.translateZ(-this.speed);
-        this.boundingBox.copy(this.sphere.geometry.boundingBox).applyMatrix4(this.sphere.matrixWorld);
     }
 }
