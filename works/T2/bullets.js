8be185e140477d89089d898380293a119a58fe86
Wagno Leao Sergio <wagnoleao@gmail.com>
2022-06-22 15:29:41 -0300

    classe EnemyBullet criada

--- works/T2/bullets.js
+++ works/T2/bullets.js
@@ -30,3 +30,41 @@ export class Bullet {
         this.boundingBox.copy(this.sphere.geometry.boundingBox).applyMatrix4(this.sphere.matrixWorld);
     }
 }
+
+{/**
+    Classe referente ao objeto que controla as
+    funcionalidades dos projetes que os inimigos atiram'.
+*/}
+export class EnemyBullet {
+    constructor(speed) {
+        this.geometry = new THREE.SphereGeometry( 0.75, 32, 32 );
+        this.material = new THREE.MeshLambertMaterial( { color: 0xffa500 } );
+        this.sphere = new THREE.Mesh( this.geometry, this.material );
+        this.speed = speed * 1.25;
+        this.boundingBox = new THREE.Box3().setFromObject(this.sphere);
+        return;
+    }
+
+    create(enemy) {
+        this.sphere.position.set(
+            enemy.cube.position.x,
+            enemy.cube.position.y,
+            enemy.cube.position.z + 5
+        );
+        return;
+    }
+
+    update() {
+        this.sphere.translateZ(this.speed);
+        this.boundingBox.copy(this.sphere.geometry.boundingBox).applyMatrix4(this.sphere.matrixWorld);
+    }
+
+    
+    //Checa colisão do objeto com o avião
+    checkPlaneCollision(plane) {
+        if(this.boundingBox.intersectsBox(plane.boundingBox)) {
+            return true;
+        }
+        return false;
+    }
+}
