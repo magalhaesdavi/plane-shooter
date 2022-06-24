a5401dbd5d09241218b273b7f3eb5a8526204583
Wagno Leao Sergio <wagnoleao@gmail.com>
2022-06-24 19:47:10 -0300

    Leia a descricao
    - criterio de permissao de tiro atualizado;
    
    - classe GroundEnemy implementada

--- works/T2/enemies.js
+++ works/T2/enemies.js
@@ -1,5 +1,5 @@
 import * as THREE from  'three';
-import { EnemyBullet } from './bullets.js'
+import { EnemyBullet, GroundAirEnemyMissile } from './bullets.js'
 // import { degreesToRadians } from '../../libs/util/util';
 
 function degreesToRadians(degrees)
@@ -42,7 +42,7 @@ export class lineEnemy {
     }
 
     shoot(speed, airplane) {
-        if(this.shootPermission){
+        if(this.shootPermission && this.cube.position.z <= airplane.getPosition().z){
             let bullet = new EnemyBullet(speed);
             bullet.create(this);
             bullet.sphere.lookAt(airplane.cone.position);
@@ -130,7 +130,7 @@ export class archEnemy {
     }
 
     shoot(speed, airplane) {
-        if(this.shootPermission){
+        if(this.shootPermission && this.cube.position.z <= airplane.getPosition().z){
             let bullet = new EnemyBullet(speed);
             bullet.create(this);
             bullet.sphere.lookAt(airplane.cone.position);
@@ -199,10 +199,77 @@ export class diagonalEnemy {
     }
 
     shoot(speed, airplane) {
-        if(this.shootPermission){
+        if(this.shootPermission && this.cube.position.z <= airplane.getPosition().z){
             let bullet = new EnemyBullet(speed);
             bullet.create(this);
             bullet.sphere.lookAt(airplane.cone.position);
+            this.shootPermission = false;
+            return bullet;
+        }
+        else {
+            return null;
+        }
+    }
+
+    //Checa colisão do objeto com todos os mísseis na tela
+    checkMissileCollision(missiles) {
+        for(var i = 0; i < missiles.length; i++) {
+            if(this.boundingBox.intersectsBox(missiles[i].boundingBox)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //Checa colisão do objeto com o avião
+    checkPlaneCollision(plane) {
+        if(this.boundingBox.intersectsBox(plane.boundingBox)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    getGeometry() {
+        return this.cube;
+    }
+}
+
+
+export class GroundEnemy {
+    constructor(speed) {
+        this.geometry = new THREE.BoxGeometry( 10, 5, 10 );
+        this.material = new THREE.MeshLambertMaterial({ color: 0x8c6d1f });
+        this.cube = new THREE.Mesh( this.geometry, this.material );
+        this.speed = speed;
+        this.boundingBox = new THREE.Box3().setFromObject(this.cube);
+        this.shootPermission = false;
+        this.startTime = new Date();
+        return;
+    }
+
+    //Define posicao do cubo
+    setPosition(x, y, z) {
+        this.cube.position.set(x, y, z)
+        return;
+    }
+
+    //Movimenta com base em sua velocidade
+    update() {
+        let endTime = new Date();
+        if(endTime - this.startTime > 2000) {
+            this.shootPermission = true;
+            this.startTime = new Date();
+        }
+        this.cube.translateZ(this.speed);
+        this.boundingBox.copy(this.cube.geometry.boundingBox).applyMatrix4(this.cube.matrixWorld);
+    }
+
+    shoot(speed, airplane) {
+        if(this.shootPermission && this.cube.position.z <= airplane.getPosition().z){
+            let bullet = new GroundAirEnemyMissile(speed);
+            bullet.create(this);
             this.shootPermission = false;
             return bullet;
         }
